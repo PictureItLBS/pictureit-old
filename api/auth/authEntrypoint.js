62e794709132713cd7ebabc08d76b57e4f573a7c
Fabian Seluska <fabianseluska@gmail.com>
2021-05-05 20:17:32 +0200

    Implemented account creation and som error handling.

--- api/auth/authEntrypoint.js
+++ api/auth/authEntrypoint.js
@@ -1,16 +1,78 @@
 import { Router } from 'express'
+import { body as validate, validationResult } from 'express-validator'
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import User from '../models/User.js'
+
 
 const authApi = Router()
 
-authApi.post('/register', (req, res) => {
-    // Read + validate the req body.
+authApi.post(
+    '/register', 
+    validate('username').isString().isAlphanumeric().isLength({ min: 1, max: 32 }),
+    validate('password').isLength({ min: 1 }),
+    async (req, res) => {
 
-    // Check if the username already exists, if so, deny the request.
+        // Check fro errors
+        const errors = validationResult(req)
 
-    // Add the user to the database:
-    //  * Salt and hash the password
-    //  * Redirect the user to the page that shows that they need to be confirmed by a teacher.
-})
+        // Return the errors to the client if there are any.
+        if (!errors.isEmpty())
+            return res.status(400).render(
+                'pages/errors/genericError.njk',
+                {
+                    errorSource: "skapande av konto",
+                    errorCode: "Account Creation Validation Failed",
+                    solution: "Den informationen du slog in kan ha innehållt icke-tillåtna tecken. (T.ex. likhetstecken i användarnamnet.)",
+                    error: errors.array().toString()
+                }
+            )
+
+        // Get the data from the body
+        const { username, password } = req.body
+
+        // Check if the username already exists; we can't have two BurnyLlamas, for example.
+        const checkForAlreadyExistingUser = await User.findOne({ name: username })
+        if (checkForAlreadyExistingUser)
+            return res.status(400).render(
+                'pages/errors/userAlreadyExists.njk',
+                {
+                    username: username
+                }
+            )
+
+        // Hash the password, because ... security.
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        // Create a new user.
+        const user = new User({
+            name: username,
+            password: hashedPassword,
+            permissionLevel: 0,
+            follows: [],
+            following: [],
+            likes: 0,
+            likedPosts: [],
+            posts: []
+        })
+
+        // Save the user in the database. If it fails send an error to the user.
+        try {
+            await user.save()
+            res.render('pages/auth/message.njk')
+        } catch (err) {
+            res.status(400).render(
+                'pages/errors/genericError.njk',
+                {
+                    errorSource: "sparande av användaren i databasen",
+                    solution: "Testa att registrera dig igen, eller kontakta PictureIt.",
+                    error: err
+                }
+            )
+        }
+    }
+)
 
 authApi.get('/signin', (req, res) => {
     // Read + validate req body.
--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 // Import 3rd party packages/libs
 import express from 'express'
+import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
 import njk from 'nunjucks'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
 
 // Import routes for API and the actual site.
 import siteRoutes from './routes/siteEntrypoint.js'
 import apiRoutes from './api/apiEntrypoint.js'
 
+
+// Load environment veriables from the '.env' file.
+dotenv.config()
+
+
 // Create an app being an instance of express - the library that handles the backend server.
 const App = express()
+
+
+// Connect to the database and shit.
+mongoose.connect(
+    process.env.DB_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    () => console.log("Database connected.")
+)
+
+
+// Configure middlewares for express. (Basically addons/plugins; extra functionality.)
+App.use(bodyParser.urlencoded({ extended: true }))
+App.use(bodyParser.json())
+App.use(cookieParser())
+
 
 // Setup nunjucks. Nunjucks uses the 'views' directory.
 // Nunjucks is a rendering engine that allows you to reuse HTML code. (And more.)
@@ -19,12 +47,15 @@ njk.configure(
     }
 )
 
+
 // Setup the static file directory 'static'. It contains non-changing assets and so on.
 App.use(express.static('static'))
+
 
 // Setup the server to use the imported routes on said paths.
 App.use('/', siteRoutes)
 App.use('/api', apiRoutes) // In the longterm you should consider binding them to versions like: /api/v1, /api/v2, etc.
+
 
 // Start the server on port 5500
 App.listen(5500, () => {
