d69017f7ff1897babd90ae93124fb46d18f74487
Fabian Seluska <fabianseluska@gmail.com>
2021-05-06 11:36:32 +0200

    Added login API.

--- api/auth/authEntrypoint.js
+++ api/auth/authEntrypoint.js
@@ -1,6 +1,5 @@
 import { Router } from 'express'
 import { body as validate, validationResult } from 'express-validator'
-import mongoose from 'mongoose'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import User from '../models/User.js'
@@ -13,8 +12,7 @@ authApi.post(
     validate('username').isString().isAlphanumeric().isLength({ min: 1, max: 32 }),
     validate('password').isLength({ min: 1 }),
     async (req, res) => {
-
-        // Check fro errors
+        // Check for errors
         const errors = validationResult(req)
 
         // Return the errors to the client if there are any.
@@ -66,6 +64,7 @@ authApi.post(
                 'pages/errors/genericError.njk',
                 {
                     errorSource: "sparande av användaren i databasen",
+                    errorCode: "Account Save to Database Failed",
                     solution: "Testa att registrera dig igen, eller kontakta PictureIt.",
                     error: err
                 }
@@ -74,14 +73,67 @@ authApi.post(
     }
 )
 
-authApi.get('/signin', (req, res) => {
-    // Read + validate req body.
+authApi.get(
+    '/login',
+    validate('username').isString().isAlphanumeric().isLength({ min: 1, max: 32 }),
+    validate('password').isLength({ min: 1 }),
+    async (req, res) => {
+        // Check for errors
+        const errors = validationResult(req)
 
-    // Check if username exists.
+        // Return the errors to the client if there are any.
+        if (!errors.isEmpty())
+            return res.status(400).render(
+                'pages/errors/genericError.njk',
+                {
+                    errorSource: "inloggning",
+                    errorCode: "Account Login Validation Failed",
+                    solution: "Den informationen du slog in kan ha innehållt icke-tillåtna tecken. (T.ex. likhetstecken i användarnamnet.) \nOm problemet återstår efter att du har försökt igen, kontakta PictureIt eller rapportera det som en bugg.",
+                    error: errors.array()
+                }
+            )
 
-    // Check if password hash matches.
+        // Get the data from the body
+        const { username, password } = req.body
 
-    // Send back a cookie with a JWT.
-})
+        // Check if the user exists in the database; if not, you can't sign in.
+        const userInDatabase = await User.findOne({ name: username })
+        if (!userInDatabase)
+            return res.status(400).render(
+                'pages/errors/userDoesNotExist.njk',
+                {
+                    username: username
+                }
+            )
+
+        const match = await bcrypt.compare(password, userInDatabase.password)
+        if (match) {
+            // Create an API token for the user that lasts one hour.
+            const userApiToken = jwt.sign(
+                {
+                    _id: userInDatabase._id,
+                    name: userInDatabase.name,
+                    permissionLevel: userInDatabase.permissionLevel
+                },
+                process.env.TOKEN_SECRET,
+                {
+                    expiresIn: '3600s',
+                }
+            )
+
+            // Create a cookie that also expires in an hour.
+            res.cookie(
+                'apiToken',
+                userApiToken,
+                {
+                    expires: new Date(Date.now() + 360000) // One hour in milliseconds.
+                }
+            )
+
+            // Send the user to the home/feed page.
+            res.redirect('/home')
+        }
+    }
+)
 
 export default authApi
