cb149b699c3228f9cf3fe35db0f136c4e8659281
Fabian Seluska <fabianseluska@gmail.com>
2021-05-07 14:24:27 +0200

    Fixed typo.

--- static/assets/scripts/icons.js
+++ static/assets/scripts/icons.js
@@ -8,7 +8,7 @@ export default function loadIcons() {
         if (loadedIcons[iconToLoad])
             return element.innerHTML = loadedIcons[iconToLoad]
 
-        const iconSource = await fetch(`/assets/icon/${iconToLoad}.svg`)
+        const iconSource = await fetch(`/assets/icons/${iconToLoad}.svg`)
         const iconCode = await iconSource.text()
 
         // Set the cache of the icon, and then add it to the element.
