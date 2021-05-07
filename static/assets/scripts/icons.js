fe9ff66f8c0451f5e8fb280888dfbbcd8ab56d7c
Fabian Seluska <fabianseluska@gmail.com>
2021-05-07 14:08:08 +0200

    Fixed a little bug.

--- static/assets/scripts/icons.js
+++ static/assets/scripts/icons.js
@@ -8,7 +8,8 @@ export default function loadIcons() {
         if (loadedIcons[iconToLoad])
             return element.innerHTML = loadedIcons[iconToLoad]
 
-        const iconCode = await fetch(`/assets/icon/${iconToLoad}.svg`)
+        const iconSource = await fetch(`/assets/icon/${iconToLoad}.svg`)
+        const iconCode = await iconSource.text()
 
         // Set the cache of the icon, and then add it to the element.
         loadedIcons[iconToLoad] = iconCode
