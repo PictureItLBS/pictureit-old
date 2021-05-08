8f7c5c70bde8fe22d32f288bb819fcc8f05dc277
Fabian Seluska <fabianseluska@gmail.com>
2021-05-08 11:14:44 +0200

    Fixed errors when missing fields for post upload.

--- api/posts/postEntrypoint.js
+++ api/posts/postEntrypoint.js
@@ -2,31 +2,61 @@ import { Router } from 'express'
 import fileUpload from '../../libs/fileUpload.js'
 import Post from '../../models/Post.js'
 import verifyToken from '../../libs/verifyToken.js'
+import { body as validate, validationResult } from 'express-validator'
+
 
 const postApi = Router()
 
-postApi.post('/new', fileUpload.single('image'), async (req, res) => {
-    const decodedToken = verifyToken(req.cookies.apiToken)
-    if (!decodedToken)
-        return res.status(401).render('pages/errors/tokenExpired.njk')
+postApi.post(
+    '/new',
+    validate('caption').isString().isLength({ min: 1, max: 512 }),
+    fileUpload.single('image'),
+    async (req, res) => {
+        const decodedToken = verifyToken(req.cookies.apiToken)
+        if (!decodedToken)
+            return res.status(401).render('pages/errors/tokenExpired.njk')
 
-    const image = req.file.buffer
+        if (!req.file)
+            return res.status(400).render(
+                'pages/errors/genericError.njk',
+                {
+                    errorSource: "uppladdning av inlägg",
+                    errorCode: "Missing Image File",
+                    solution: "Det ser ut som att du glömde att ladda upp en bild.",
+                    error: null
+                }
+            )
 
-    const post = new Post({
-        publisher: decodedToken._id,
-        image: {
-            data: image,
-            contentType: 'image/png'
-        },
-        caption: req.body.caption,
-        date: new Date(Date.now()),
-        likedBy: []
-    })
+        const errors = validationResult(req)
+        if (!errors.isEmpty())
+            return res.status(400).render(
+                'pages/errors/genericError.njk',
+                {
+                    errorSource: "skapande av konto",
+                    errorCode: "Account Creation Validation Failed",
+                    solution: "Den informationen du slog in kan ha innehållt icke-tillåtna tecken. (T.ex. likhetstecken i användarnamnet.)",
+                    error: errors.array().toString()
+                }
+            )
 
-    const savedPost = await post.save()
+        const image = req.file.buffer
 
-    res.redirect('/app/post/view/' + savedPost._id)
-})
+        const post = new Post({
+            publisher: decodedToken._id,
+            image: {
+                data: image,
+                contentType: 'image/png'
+            },
+            caption: req.body.caption,
+            date: new Date(Date.now()),
+            likedBy: []
+        })
+
+        const savedPost = await post.save()
+
+        res.redirect('/app/post/view/' + savedPost._id)
+    }
+)
 
 postApi.get('/show', async (req, res) => {
     const posts = await Post.find({})
