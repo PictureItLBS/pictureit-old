0c5d2a86cb3889a689aba90e211177696731119b
Fabian Seluska <fabianseluska@gmail.com>
2021-05-14 22:55:47 +0200

    Posts' publishers are now clickable.

--- static/assets/scripts/explore.js
+++ static/assets/scripts/explore.js
@@ -2,7 +2,15 @@ import loadPfps from "./profilePictures.js"
 
 loadPfps('.user-pfp')
 
+// Make users in user-results from search clickable.
 document.querySelectorAll('.user').forEach(element => {
+    element.addEventListener('click', event => {
+        window.location.href = `${window.location.origin}/app/profile/user/${element.id}`
+    })
+})
+
+// Make post publishers clickable
+document.querySelectorAll('.post-publisher').forEach(element => {
     element.addEventListener('click', event => {
         window.location.href = `${window.location.origin}/app/profile/user/${element.id}`
     })
