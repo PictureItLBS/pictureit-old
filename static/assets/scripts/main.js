ef5c95c1f938f1dd5509ff309568d8025a651000
Fabian Seluska <fabianseluska@gmail.com>
2021-05-22 00:14:25 +0200

    Finilised upload! :D

--- static/assets/scripts/main.js
+++ static/assets/scripts/main.js
@@ -1,10 +1,12 @@
 import loadIcons from './icons.js'
 import initPosts from './posts.js'
 import initQuickies from './quickies.js'
+import initUpload from './upload.js'
 
 initPosts()
 loadIcons()
 initQuickies()
+initUpload()
 
 if (window.location.pathname.includes('app')) {
     const currentView = document.body.getAttribute("view")
